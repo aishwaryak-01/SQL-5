5f3cafa5a2e109593b559a9a403f68b2b3307b5f
aishwaryakb-01 <snigdha0119@gmail.com>
2023-10-09 11:45:01 +0530

    Sequelize-2

---
+++ README.md
@@ -0,0 +1,2 @@
+# SQL-2
+Creating table, Inserting-Updating-Getting all products in the table, Edit & Delete Functionalities.
---
+++ routes/admin.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const adminController = require('../controllers/admin');
+const router = express.Router();
+router.get('/add-product', adminController.getAddProducts);
+router.get('/products', adminController.getProducts);
+router.post('/add-product', adminController.postAddProducts);
+router.get('/edit-product/:productId', adminController.getEditProducts);
+router.post('/edit-product', adminController.postEditProducts);
+router.post('/delete-product', adminController.postDeleteProducts);
+module.exports = router;
